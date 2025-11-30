b4b3cd0c1e9cf759ed3702b475eae02f561117a8
RCosta210606 <rodrigofcosta7@gmail.com>
2025-11-30 00:08:42 +0000

    Increase attack damage for Spider and Zombie classes

--- src/main/java/jogo/gameobject/character/Spider.java
+++ src/main/java/jogo/gameobject/character/Spider.java
@@ -2,7 +2,7 @@ package jogo.gameobject.character;
 
 public class Spider extends Enemy{
     public Spider(String name) {
-        super(name, 8, 2.0f, 3.0f);
+        super(name, 10, 2.0f, 3.0f);
         setHealth(50);
         setMaxHealth(50);
     }
